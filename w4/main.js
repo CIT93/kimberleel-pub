6dcfa8ff6bd87de4e424e6b812d320c4279d68cb
Kimberlee Linthicum <fromkimwithlove@gmail.com>
2024-01-28 13:08:18 -0800

    setup for code-along complete

--- w4/main.js
+++ w4/main.js
@@ -41,13 +41,22 @@ function start(householdMembers, houseSize) {
 function displayOutput() {
   for (arr of cfpData) {
     const output = document.getElementById("output");
+
+    const newH2 = document.createElement("h2");
+    newH2.textContent = `Your Carbon Footprint is ${arr[4]}`;
+    const newH3 = document.createElement("h3");
+    newH3.textContent = "How your Carbon Footprint is calculated:";
     const newP = document.createElement("p");
-    newP.textContent = `A household size of ${arr[0]} adds ${arr[2]} points to your Carbon Footprint. If you live in a(n) ${arr[1]} size home, add ${arr[3]} points.  Your total Carbon Footprint, thus far, is ${arr[4]} points.`;
+    newP.textContent = `A household size of ${arr[0]} adds ${arr[2]} points to your Carbon Footprint.`;
+    newP.textContent += ` Then add ${arr[3]} points if your live in a(n) ${arr[1]} home.`;
+    newP.style.borderBottom = "1px solid gray";
+
+    output.appendChild(newH2);
+    output.appendChild(newH3);
     output.appendChild(newP);
   }
 }
 
-start(1, "apartment");
 start(2, "small");
 start(3, "medium");
 start(4, "large");
